07fae653243788d2d2e0c8627a9f309705eabf52
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-12 11:31:17 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { resolve } from 'path';
 
 import routes from './routes';
@@ -15,6 +16,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
