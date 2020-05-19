4d2fa060bdd154ff4d83a04085abb7658a623c13
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-05-19 20:11:06 -0300

    adjust/list deliveries with problems

--- src/app/controllers/DeliveryWithProblemController.js
+++ src/app/controllers/DeliveryWithProblemController.js
@@ -9,49 +9,40 @@ class DeliveryWithProblemController {
   async index(req, res) {
     const { page = 1, limit = 10 } = req.query;
 
-    const DeliveriesWithProblem = await DeliveryProblem.aggregate(
-      'delivery_id',
-      'DISTINCT',
-      {
-        plain: false,
-        where: {
-          delivery_id: { [Op.not]: null },
-        },
-      }
-    );
-
-    const ordersId = DeliveriesWithProblem.map(delivery => delivery.DISTINCT);
-
-    const orders = await Order.findAll({
-      where: {
-        id: ordersId,
-      },
+    const DeliveriesWithProblem = await DeliveryProblem.findAll({
       limit,
       offset: (page - 1) * limit,
-      attributes: ['id', 'product', 'start_date', 'end_date'],
+      attributes: ['id', 'description'],
       include: [
         {
-          model: Recipient,
-          as: 'recipient',
-          attributes: [
-            'id',
-            'name',
-            'cep',
-            'city',
-            'street',
-            'number',
-            'complement',
+          model: Order,
+          as: 'order',
+          attributes: ['id', 'product', 'start_date', 'end_date'],
+          include: [
+            {
+              model: Recipient,
+              as: 'recipient',
+              attributes: [
+                'id',
+                'name',
+                'cep',
+                'city',
+                'street',
+                'number',
+                'complement',
+              ],
+            },
+            {
+              model: File,
+              as: 'signature',
+              attributes: ['id', 'path', 'url'],
+            },
           ],
-        },
-        {
-          model: File,
-          as: 'signature',
-          attributes: ['id', 'path', 'url'],
         },
       ],
     });
 
-    return res.json(orders);
+    return res.json(DeliveriesWithProblem);
   }
 }
 
